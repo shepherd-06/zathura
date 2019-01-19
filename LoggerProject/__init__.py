50a42becec3de9b9de9fbe2e9866daced4aeb0e6
piraaaaaaaat <Ibtehaz.shawon@gmail.com>
2019-01-19 18:07:19 +0600

    error reporting created by user

--- LoggerProject/__init__.py
+++ LoggerProject/__init__.py
@@ -6,7 +6,7 @@ from git import Repo
 
 def create_app():
     # It should be hardcode False on production
-    known_commands = ('v', 'db_test')
+    known_commands = ('v', 'db_insert', 'db_get_all', 'db_user')
     if len(sys.argv) > 1:
         for args in sys.argv[1:]:        
             if args in known_commands:
@@ -17,13 +17,33 @@ def create_app():
                         print(tags[-1])
                     else:
                         print("ERROR! version name not found.")
-                elif args == 'db_test':
+                elif args == 'db_insert':
                     from .sqlite_utility import Sqlite_Utility
                     sql_utils = Sqlite_Utility()
                     rows = sql_utils.insert_error_log(user="test123", error_name="No error", error_description="no description", point_of_origin=create_app.__name__)
                     print("error inserted test: {}".format(rows))
                     debug_rows = sql_utils.insert_debug_log(user="test123", message_data="eiuhsodfdf bkisdjsdf jsbjlsdfd", point_of_origin=create_app.__name__)
                     print("debug rows added {}".format(debug_rows))
+                elif args == 'db_get_all':
+                    from .sqlite_utility import Sqlite_Utility
+                    sql_utils = Sqlite_Utility()
+                    all_error_logs = sql_utils.get_all_error_log()
+                    all_debug_logs = sql_utils.get_all_debug_log()
+
+                    print("All error logs")
+                    print(all_error_logs)
+                    print("-----------------------------")
+                    print("-----------------------------")
+                    print("All debug logs")
+                    print(all_debug_logs)
+                    print("-----------------------------")
+                    print("-----------------------------")
+                elif args == "db_user":
+                    from .sqlite_utility import Sqlite_Utility
+                    sql_utils = Sqlite_Utility()
+                    user = input("Enter a username: ")
+                    logs = sql_utils.get_error_by_user(user)
+                    print(logs)
             else:
                 print("unknown command - {}".format(args))
                 break
--- LoggerProject/sqlite_utility.py
+++ LoggerProject/sqlite_utility.py
@@ -7,7 +7,7 @@ class Sqlite_Utility:
         # TODO: should check if database is already connected or not.
         database_connection()  # initiate database connection before doing anything. 
 
-    def insert_error_log(self, user, error_name, error_description, point_of_origin = None) -> int:
+    def insert_error_log(self, user, error_name, error_description, point_of_origin = None):
         if user is not None and error_name is not None and error_description is not None:
             error_log = ErrorLog(user = user, error_name = error_name, error_description = error_description, point_of_origin = point_of_origin)
             return error_log.save()  # number of modified rows are returned. (Always be 1)
@@ -22,7 +22,7 @@ class Sqlite_Utility:
             print("unknown error occurred")
             return 0
             
-    def insert_debug_log(self, user, message_data, point_of_origin=None) -> int:
+    def insert_debug_log(self, user, message_data, point_of_origin=None):
         if user is not None and message_data is not None:
             debug_log = DebugLog(user=user, message_data = message_data, point_of_origin = point_of_origin)
             return debug_log.save()
@@ -33,3 +33,52 @@ class Sqlite_Utility:
                 print("message_data should not be NONE. Since it's what you are currently logging to remember right?")
             print("unknown error occurred")
             return 0
+
+    def __error_obj_to_dict(self, error_log_object: ErrorLog):
+        return {
+            '_id': error_log_object._id,
+            'user': error_log_object.user,
+            'error_name': error_log_object.error_name,
+            'error_description': error_log_object.error_description,
+            'point_of_origin': error_log_object.point_of_origin,
+            'logged_at': error_log_object.logged_at,
+            'is_resolved': error_log_object.is_resolved,
+            'resolved_at': error_log_object.resolved_at,
+        }
+
+    def __debug_obj_to_dict(self, debug_log_object: DebugLog):
+        return {
+            "_id": debug_log_object._id,
+            "user": debug_log_object.user,
+            "message-data": debug_log_object.message_data,
+            "point_of_origin": debug_log_object.point_of_origin,
+            "logged_at": debug_log_object.logged_at,
+        }
+
+    def get_all_error_log(self):
+        # returns all error_log table data on a list
+        err_logs = ErrorLog.select()
+        all_logs = list()
+        for logs in err_logs:
+            all_logs.append(self.__error_obj_to_dict(logs))
+        all_logs.append({"total": len(all_logs)})
+        return all_logs
+
+    def get_all_debug_log(self):
+        # returns all debug_log table data on a list
+        debug_logs = DebugLog.select()
+        all_logs = list()
+        for log in debug_logs:
+            all_logs.append(self.__debug_obj_to_dict(log))
+        all_logs.append({"total": len(all_logs)})
+        return all_logs
+
+    def get_error_by_user(self, user):
+        # returns error generated for a user
+        errors = ErrorLog.select().where(ErrorLog.user == user.split())
+        all_error_logs = list()
+        for err in errors:
+            all_error_logs.append(self.__error_obj_to_dict(err))
+        all_error_logs.append({"total": len(all_error_logs)})
+        return all_error_logs
+
