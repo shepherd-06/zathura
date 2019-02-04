4967af077c70fbee6c6956279274b47a49cac859
piraaaaaaaat <Ibtehaz.shawon@gmail.com>
2019-02-04 17:05:31 +0600

    upgrade to v0.0.3-dev7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
 
 setup(name='zathura',
       packages = ['ZathuraProject'],
-      version='0.0.3.dev6',
+      version='0.0.3.dev7',
       description='Zathura is a small space logger. It logs stuff into her logbook as she drifts into vast unknown of development phase.',
       url='https://github.com/ibtehaz-shawon/Zathura',
       author='Ibtehaz Shawon',
